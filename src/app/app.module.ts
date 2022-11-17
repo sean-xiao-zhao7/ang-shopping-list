d4a8c0e2db1e8e4b1782539d45a3bd21ac4e3be0
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-11-17 16:16:02 -0500

    Add forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -37,6 +38,7 @@ import { RecipeListItemComponent } from './recipe-book/recipe-list-item/recipe-l
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatButtonModule,
     MatCardModule,
     MatGridListModule,
